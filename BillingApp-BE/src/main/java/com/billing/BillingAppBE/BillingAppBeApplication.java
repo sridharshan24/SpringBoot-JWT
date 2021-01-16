87a2cc92f48eff8fae980dec30797adfdd89e304
sridh <sridhar.shanu24@gmail.com>
2021-01-16 19:12:47 +0530

    Initial commit for Backend

---
+++ BillingApp-BE/src/main/java/com/billing/BillingAppBE/BillingAppBeApplication.java
@@ -0,0 +1,13 @@
+package com.billing.BillingAppBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingAppBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingAppBeApplication.class, args);
+	}
+
+}
---
+++ BillingApp-BE/src/test/java/com/billing/BillingAppBE/BillingAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billing.BillingAppBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingAppBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
